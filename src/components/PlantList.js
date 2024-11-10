e35f7b7bdb83480da213db2c4bf86a333daa80ae
KesiBrian <briankesim@gmail.com>
2024-11-11 00:19:48 +0300

    origin

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,36 +1,14 @@
-import React, { useState, useEffect } from "react";
-import NewPlantForm from "./NewPlantForm";
-import PlantList from "./PlantList";
-import Search from "./Search";
+import React from "react";
+import PlantCard from "./PlantCard";
 
-function PlantPage() {
-  const [plants, setPlants] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
-
-  // Fetch plants from db.json when the component mounts
-  useEffect(() => {
-    fetch("http://localhost:3000/plants")
-      .then((response) => response.json())
-      .then((data) => setPlants(data));
-  }, []);
-
-  // Callback to add a new plant to the list
-  function handleAddPlant(newPlant) {
-    setPlants([...plants, newPlant]);
-  }
-
-  // Filter plants based on search term
-  const displayedPlants = plants.filter((plant) =>
-    plant.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
-
+function PlantList({ plants }) {
   return (
-    <main>
-      <NewPlantForm onAddPlant={handleAddPlant} />
-      <Search searchTerm={searchTerm} onSearchChange={setSearchTerm} />
-      <PlantList plants={displayedPlants} />
-    </main>
+    <ul className="cards">
+      {plants.map((plant) => (
+        <PlantCard key={plant.id} plant={plant} />
+      ))}
+    </ul>
   );
 }
 
-export default PlantPage;
+export default PlantList;
